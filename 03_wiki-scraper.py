15ed88c885b57e08f18e63182e70ad363d940d21
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2026-02-17 09:35:54 +0000

    finished exercise 03

--- 03_wiki-scraper.py
+++ 03_wiki-scraper.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from urllib.pare import urljoin
+from urllib.parse import urljoin
 import requests
 
 
@@ -27,3 +27,42 @@ print(titles)
 links = [urljoin(url, link.get('href')) for link in soup.find_all('a')]
 
 print(links)
+
+# extract all content
+# all_text = soup.get_text()
+# print(all_text)
+
+# extract text from main elements
+# main_text = soup.find('main').get_text()
+# print(main_text)
+
+# extract content with an ID
+# content_text = soup.find('div', {'id': 'mw-content-text'}).get_text()
+# print(content_text)
+
+
+# Open graph -------------------------------------------------------
+
+url = "https://midu.dev"
+
+# we can use custom headers
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.7119.0 Mobile Safari/537.36 (compatible; Googlebot/2.1; http://www.google.com/bot.html)'
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    print("success !")
+
+
+# extract open graph if exist
+og_image = soup.find('meta', {'property': 'og:image'})
+
+# alternative
+og_image = soup.find('meta', property='og:image')
+
+if og_image:
+    print(og_image['content'])
+else:
+    print('Image not found')
