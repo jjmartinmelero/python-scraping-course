7dda059050eb24277404a434b90072ebe5ead6fa
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2026-02-17 14:04:53 +0000

    updated seo file

--- 04_seo_cli.py
+++ 04_seo_cli.py
@@ -9,8 +9,6 @@ parser.add_argument('url', type=str, help="the url of the site you want scrape a
 args = parser.parse_args()
 url = args.url
 
-url = "https://es.wikipedia.org/wiki/Web_scraping"
-
 # we can use custom headers
 headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.7119.0 Mobile Safari/537.36 (compatible; Googlebot/2.1; http://www.google.com/bot.html)'
@@ -18,5 +16,35 @@ headers = {
 
 response = requests.get(url, headers=headers)
 
+print(args.url)
+
+print(response)
+
 if response.status_code == 200:
     print("success !")
+
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+print(f"\033[34mReview the page: {url}\033[0m")
+print("\n SEO:")
+
+title_page = soup.title.string
+
+if title_page:
+    print(f"The title is {title_page}")
+    if len(title_page) < 70:
+        print("The title has more than 70 characters.")
+    else:
+        print("The title is too long")
+
+
+# Extract all h1 elements
+titles = [title.string for title in soup.find_all('h1')]
+
+if not titles: 
+    print("Titles not found")
+elif len(titles) > 1: 
+    print("more than 1 title")
+else:
+    print("only 1 title found")
